9d95197b4f1ec8ecfd77a9301cd87022baa8ede6
Joxeph <codewithjoxeph@gmail.com>
2024-10-03 14:13:53 +0100

    Fixed dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,27 +1,25 @@
-import useDashboard from "../features/dashboard/useDashboard";
+import useFetchCertificates from "../features/certificates/useFetchCertificates";
 import ErrorPage from "../ui/ErrorPage";
 import Loader from "../ui/Loader";
 import TableList from "../ui/TableList";
 
 function Dashboard() {
-const {data, isLoading, error, refetch} = useDashboard()
-
+  const { certificates, isLoading, error, refetch } = useFetchCertificates();
 
   if (isLoading) return <Loader />;
 
-  if (!isLoading && error) return <ErrorPage onClick={() => refetch()} />
+  if (!isLoading && error) return <ErrorPage onClick={() => refetch()} />;
 
   return (
     <div className="p-2 space-y-3">
       <h1 className="font-semibold text-2xl">Hello Kolawunla</h1>
       <p className="text-sm text-gray-500">Welcome Back</p>
 
-
       <h2 className="font-semibold">Recently Uploaded</h2>
       <div className="px-4 flex flex-col gap-4">
-        {/* {data?.map((item) => (
+        {certificates?.slice(0, 5).map((item) => (
           <TableList key={item.id} data={item} />
-        ))} */}
+        ))}
       </div>
     </div>
   );
